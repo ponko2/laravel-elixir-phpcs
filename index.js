b363b0c7a9cb5c8c001dd36601b5d435ca4a13e5
Takahito Nakano <kano@ponko2.jp>
2015-10-12 02:37:53 +0900

    Laravel Elixir v3.x対応

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 'use strict';
 
 var gulp        = require('gulp');
-var elixir      = require('laravel-elixir');
-var notify      = require('gulp-notify');
+var Elixir      = require('laravel-elixir');
 var phpcs       = require('gulp-phpcs');
 var gutil       = require('gulp-util');
 var map         = require('map-stream');
 var events      = require('events');
 var _           = require('underscore');
-var path        = require('path');
 var colors      = gutil.colors;
 var emitter     = new events.EventEmitter();
 var PluginError = gutil.PluginError;
+var notify      = new Elixir.Notification;
+var config      = Elixir.config;
 
-var logReporter = function() {
-  return map(function(files, cb) {
-    _.each(files, function(file) {
+var logReporter = function () {
+  return map(function (files, cb) {
+    _.each(files, function (file) {
       var report = file.phpcsReport || {};
 
       if (!report.error) {
@@ -26,21 +26,21 @@ var logReporter = function() {
         colors.red(report.error) + '\n    ' +
         report.output.replace(/\n/g, '\n    ');
 
-      console.log(message);
+      gutil.log(message);
     });
 
     cb(null, files);
   });
 };
 
-var failReporter = function() {
-  return map(function(files, cb) {
-    var errors = _.filter(files, function(file) {
+var failReporter = function () {
+  return map(function (files, cb) {
+    var errors = _.filter(files, function (file) {
       return file.phpcsReport.error;
     });
 
     if (errors.length > 0) {
-      var fails = _.map(errors, function(file) {
+      var fails = _.map(errors, function (file) {
         return file.path;
       });
 
@@ -53,38 +53,24 @@ var failReporter = function() {
   });
 };
 
-elixir.extend('phpcs', function(src, options) {
-  src = src || 'app/**/*.php';
+Elixir.extend('phpcs', function (src, options) {
+  var paths = new Elixir.GulpPaths()
+    .src(src || [config.appPath + '/**/*.php']);
 
-  options = options || {};
+  new Elixir.Task('phpcs', function () {
+    this.log(paths.src);
 
-  var onError = function(err) {
-    notify.onError({
-      title: 'Laravel Elixir',
-      subtitle: 'PHP_CodeSniffer failed.',
-      message: '<%= error.message %>',
-      icon: path.join(__dirname, '../laravel-elixir/icons/fail.png')
-    })(err);
+    var onError = function (err) {
+      notify.error(err, 'PHP_CodeSniffer failed');
+      this.emit('end');
+    };
 
-    this.emit('end');
-  };
-
-  gulp.task('phpcs', function() {
-    return gulp.src(src)
-      .pipe(phpcs(options))
+    return gulp.src(paths.src.path)
+      .pipe(phpcs(options || {}))
       .pipe(gutil.buffer())
       .pipe(logReporter())
       .pipe(failReporter())
       .on('error', onError)
-      .pipe(notify({
-        title: 'Laravel Elixir',
-        subtitle: 'PHP_CodeSniffer passed.',
-        message: ' ',
-        icon: path.join(__dirname, '../laravel-elixir/icons/pass.png')
-      }));
-  });
-
-  this.registerWatcher('phpcs', src);
-
-  return this.queueTask('phpcs');
+      .pipe(notify.message('PHP_CodeSniffer passed'));
+  }).watch(paths.src.path);
 });
