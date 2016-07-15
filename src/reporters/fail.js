fcbc3cd5c09a1d157a83c730614705c247751aae
Takahito Nakano <kano@ponko2.jp>
2016-07-15 17:19:07 +0900

    エラーメッセージを調整

--- src/reporters/fail.js
+++ src/reporters/fail.js
@@ -1,21 +1,17 @@
 const gutil       = require('gulp-util');
 const map         = require('map-stream');
 const events      = require('events');
+const pluralize   = require('pluralize');
 const emitter     = new events.EventEmitter();
 const PluginError = gutil.PluginError;
 
 module.exports = function () {
   return map((files, cb) => {
-    let fails   = [];
-    let message = '';
-
     const errors = files.filter(file => file.phpcsReport.error);
 
     if (errors.length > 0) {
-      fails   = errors.map(file => file.path);
-      message = fails.join(', ');
-
-      emitter.emit('error', new PluginError('phpcs', message));
+      const errorMessage = `Failed with ${errors.length} ${pluralize('error', errors.length)}`;
+      emitter.emit('error', new PluginError('phpcs', errorMessage));
     }
 
     cb(null, files);
